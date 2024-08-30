e695d3fb3ee775e6ebbce8d5075fbbe1a045eba2
macbook <macbook@MacBook-Air-ihajar.local>
2024-08-30 12:13:01 +0100

    11: messages editor

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -5,7 +5,9 @@ import { UseGetChannel } from "@/features/channels/api/use-get-channel";
 import { useChannelId } from "@/hooks/use-channel-id";
 
 import { Loader, TriangleAlert } from "lucide-react";
+
 import { Header } from "./header";
+import { ChatInput } from "./chat-input";
 
 
 const ChannelIdPage = () => {
@@ -35,6 +37,8 @@ const ChannelIdPage = () => {
     return ( 
         <div className="flex flex-col h-full">
             <Header title={channel.name} />
+            <div className="flex-1" />
+            <ChatInput placeholder={`Message # ${channel.name}`} />
         </div>
     );
 }
