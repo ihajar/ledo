5e7fb6738d5b47c871a4338050b59e20de19e56e
macbook <macbook@MacBook-Air-ihajar.local>
2024-08-30 09:57:45 +0100

    9: updating and deleting channel api with modals

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,9 +1,42 @@
+"use client";
+
+import { UseGetChannel } from "@/features/channels/api/use-get-channel";
+
+import { useChannelId } from "@/hooks/use-channel-id";
+
+import { Loader, TriangleAlert } from "lucide-react";
+import { Header } from "./header";
+
+
 const ChannelIdPage = () => {
+    const channelId = useChannelId();
+
+    const { data: channel, isLoading: channelLoading } = UseGetChannel({ id: channelId });
+
+    if(channelLoading) {
+        return (
+            <div className="h-full flex-1 flex items-center justify-center">
+                <Loader className="size-5 animate-spin text-muted-foreground" />
+            </div>
+        );
+    };
+
+    if(!channel) {
+        return (
+            <div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center">
+                <TriangleAlert className="size-5 text-muted-foreground" />
+                <span className="text-sm text-muted-foreground">
+                    Channel not found
+                </span>
+            </div>
+        );
+    };
+
     return ( 
-        <div>
-            Channel ID Page
+        <div className="flex flex-col h-full">
+            <Header title={channel.name} />
         </div>
     );
 }
  
-export default ChannelIdPage;
+export default ChannelIdPage; 
