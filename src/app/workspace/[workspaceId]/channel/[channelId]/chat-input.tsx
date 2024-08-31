d44adaed8095060e88d13ba01745e2ef71c8ee1a
macbook <macbook@MacBook-Air-ihajar.local>
2024-08-31 17:39:07 +0100

    13: upload image files to convex db

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,29 +1,47 @@
 import dynamic from "next/dynamic";
+import { useRef, useState } from "react";
 
-import { useRef } from "react";
-
+import { toast } from "sonner";
 import Quill from "quill";
 
+import { useGenerateUploadUrl } from "@/features/upload/api/use-generate-upload-url";
 import { useCreateMessage } from "@/features/messages/api/use-create-message";
 
 import { useWorkspaceId } from "@/hooks/use-worspace-id";
 import { useChannelId } from "@/hooks/use-channel-id";
 
+import { Id } from "../../../../../../convex/_generated/dataModel";
+
+
 
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
+
 
 interface ChatInputProps {
     placeholder: string;
 };
 
+type CreateMessageValues = {
+    channelId: Id<"channels">;
+    workspaceId: Id<"workspaces">;
+    body: string;
+    image?: Id<"_storage"> | undefined;
+};
+
 export const ChatInput = ({ placeholder }: ChatInputProps) => {
+    const [editorKey, setEditorKey] = useState(0);
+    const [isPending, setIsPending] = useState(false);
+
     const editorRef = useRef<Quill | null>(null);
 
     const channelId = useChannelId();
     const workspaceId = useWorkspaceId();
+
+    const { mutate: generateUpalodUrl } = useGenerateUploadUrl();
+
     const { mutate: createMessage } = useCreateMessage();
 
-    const handleSubmit = ({
+    const handleSubmit = async({
         body,
         image
     }: {
@@ -31,21 +49,57 @@ export const ChatInput = ({ placeholder }: ChatInputProps) => {
         image: File | null;
 
     }) => {
-        console.log({ body, image });
+        try {
+            setIsPending(true);
+            editorRef?.current?.enable(false);
 
-        createMessage({
-            workspaceId,
-            channelId,
-            body,
-        });
+            const values: CreateMessageValues = {
+                channelId,
+                workspaceId,
+                body,
+                image: undefined
+            };
+
+            if(image) {
+                const url = await generateUpalodUrl({}, { throwError: true });
+               
+                if(!url) {
+                    throw new Error("Url not found");
+                }
+
+                const result = await fetch(url, {
+                    method: "POST",
+                    headers: { "Content-Type": image.type },
+                    body: image,
+                });                
+
+                if(!result.ok) {
+                    throw new Error("Failed to upload image");
+                }
+
+                const { storageId } = await result.json();
+
+                values.image = storageId;
+            }
+
+            await createMessage(values, { throwError: true });
+
+            setEditorKey((prevKey) => prevKey + 1);
+        } catch (error) {
+            toast.error("Failed to send message");
+        } finally {
+            setIsPending(false);
+            editorRef?.current?.enable(true);
+        }
     };
 
     return (
         <div className="px-5 w-full">
             <Editor
+                key={editorKey}
                 placeholder={placeholder}
                 onSubmit={handleSubmit}
-                disabled={false}
+                disabled={isPending}
                 innerRef={editorRef}
             />
         </div>
